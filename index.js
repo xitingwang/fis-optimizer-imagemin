e6eb7ef938878ca89f1ce81c91ddfe3d2b564c65
xitingwang <wang234@gmail.com>
2016-08-24 13:12:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
- * @Author: wangchao
- * @Date:   2016-08-23 20:03:03
- * @Last Modified by:   wangchao
- * @Last Modified time: 2016-08-24 00:05:57
- */
-
 'use strict';
 const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
@@ -14,7 +7,13 @@ const imageminSvgo = require('imagemin-svgo');
 
 module.exports = function(content, file, conf) {
 	imagemin([file.subpath.substr(1)], conf.to, {
-		use: [imageminMozjpeg(), imageminPngquant(), imageminGifsicle(), imageminSvgo()]
+		use: [imageminMozjpeg({
+			quality: 98
+		}), imageminPngquant({
+			quality: '80-85'
+		}), imageminGifsicle({
+			optimizationLevel: 3
+		}), imageminSvgo()]
 	}).then(() => {
 		//console.log('Images optimized');
 	});
